3eccdad6604b733704029dd41bd602e2db7c3dbf
fehzitu <fehzitujsdev@gmail.com>
2025-12-09 22:49:43 -0300

    fix floor hitbox too

--- src/objects/floor.js
+++ src/objects/floor.js
@@ -4,14 +4,12 @@ import { k } from '../kaboomLoader.js';
 export function createFloor() {
     return k.add([
         // rect is to make a rectangle
-        rect(width() + 8, 32),
+        k.rect(750, 16),
         // on pos the first num is to horizontal, and the second is to vertical
-        pos(- 4, height() - 32),
-        // simple outline border
-        outline(4),
-        area(),
+        k.pos(-50, 150),
+        k.area(),
         // color is only used if the object don't have sprite
-        color(127, 200, 255),
-        body({ isStatic: true })
+        k.color(127, 200, 255),
+        k.body({ isStatic: true })
     ]);
 };
